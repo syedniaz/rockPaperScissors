69b9b7d94adfb222267741262fd91e0b4725ac99
syedniaz <snm051110@gmail.com>
2023-11-03 20:37:02 +0600

    user prompt is taken and results shown on console

--- javascript.js
+++ javascript.js
@@ -2,20 +2,23 @@ function getComputerChoice () {
     let choice = Math.floor(Math.random() * 3);
     if (choice == 0) return "rock";
     else if (choice == 1) return "paper";
-    else return "scissor";
+    else return "scissors";
 }
 
 function playRound(playerSelection, computerSelection) {
     let ps = 0, cs = 0;
 
-    // rock = 0, paper = 1, scissor = 2
+    playerSelection = playerSelection.toLowerCase();
+
+    // rock = 0, paper = 1, scissors = 2
     if (playerSelection == "rock") ps = 0;
     else if (playerSelection == "paper") ps = 1;
-    else if (playerSelection == "scissor") ps = 2;
+    else if (playerSelection == "scissors") ps = 2;
+    else return "Invalid choice, please try again";
 
     if (computerSelection == "rock") cs = 0;
     else if (computerSelection == "paper") cs = 1;
-    else if (computerSelection == "scissor") cs = 2;
+    else if (computerSelection == "scissors") cs = 2;
 
     if (cs == ps) return "Draw! Another round";
 
@@ -25,4 +28,11 @@ function playRound(playerSelection, computerSelection) {
     else if (ps == 1 && cs == 2) return "Scissors beats Paper, You lose!"
     else if (ps == 2 && cs == 0) return "Rock beats Scissors, You lose!"
     else if (ps == 2 && cs == 1) return "Scissors beats Paper, You Win!"
+}
+
+function game (playerSelection) {
+    let i = 0;
+    for (i=0; i < 5; i++){
+        console.log(playRound(playerSelection, getComputerChoice()));
+    }
 }
